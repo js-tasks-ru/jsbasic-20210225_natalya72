fc56c0802fb1dbf4468b60a64ffb0288cc5dabe1
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2021-03-12 14:59:27 +0300

    Update task.test.js

--- 1-module/2-task/task.test.js
+++ 1-module/2-task/task.test.js
@@ -1,7 +1,4 @@
 describe('1-module-2-task', () => {
-  it('если имя корректное, то true', () => {
-    expect(isValid('Ilia')).toEqual(true);
-  });
 
   it('если содержит пробелы, то false', () => {
     expect(isValid('Ilia Burlak')).toEqual(false);
