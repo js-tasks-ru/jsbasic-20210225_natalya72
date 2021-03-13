9dc54c63b6290accf4c1fd29865e7a551031f446
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2021-03-13 17:00:36 +0300

    Update index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,12 +1,9 @@
 function sumSalary(salaries) {
   let sum = 0;
   for (let key in salaries) {
-    if (typeof salaries[key] !== NaN || typeof salaries[key] !== Infinity || typeof salaries[key] !== -Infinity) {
-      if (typeof salaries[key] == 'number'){
-        sum += salaries[key];
-      }
+    if (isFinite(salaries[key])){
+      sum += salaries[key];
     }
   }
   return sum;
-
 }
