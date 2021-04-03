e0e9c5d89a67ad69d6293aacf02de4fde350018d
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2021-04-04 01:13:51 +0300

    Update index.js

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,4 +1,9 @@
 function makeFriendsList(friends) {
-  console.log(1)
-  return `<ul>{friends}</ul>`;
+  const ul = document.createElement('ul');
+
+  ul.innerHTML = friends.map(item => `
+      <li>${item.firstName} ${item.lastName}</li>
+  `).join('');
+
+  return ul;
 }
