3b931c9f79101aa8dff58075db59896aa2e44ab8
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2021-04-04 02:35:10 +0300

    Update index.js

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,14 @@
 function highlight(table) {
-  // ваш код...
+  for(let i=0; i < table.rows.length; i++){
+    let row = table.rows[i];
+      for(let j=0; j < row.cells.length; j++){
+
+        if(row.cells[j].hasAttibute("data-available")){
+          row.cells[i].hidden
+        }
+        // row.cells[j]
+    }
+  }
+  // document.querySelector('[data-available]').classList.add("available");
+
 }
