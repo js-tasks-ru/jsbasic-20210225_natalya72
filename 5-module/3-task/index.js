916bd180949141c6c2f9d0abac34e0a02d3b713e
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2021-04-13 02:40:17 +0300

    Update index.js

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,23 @@
 function initCarousel() {
-  // ваш код...
+
+
+  let right = document.querySelector('.carousel__arrow_right');
+  let left = document.querySelector('.carousel__arrow_left');
+
+  // let images = document.querySelectorAll('.carousel__inner .carousel__slide .carousel__img');
+  let carousel = document.querySelector('.carousel__inner');
+
+  // let i = 0;
+
+  // let position = 0;
+
+  right.addEventListener('click', () => {
+    carousel.style.transform = 'translateX(-988px)';
+  });
+
+  left.addEventListener('click', () => {
+    carousel.style.transform = 'translateX(988px)';
+  });
+
 }
+
